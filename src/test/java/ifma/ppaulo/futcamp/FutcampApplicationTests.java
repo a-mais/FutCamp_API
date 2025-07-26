a6953f9adcd38aa45bebf05098d0880d4a1b0e67
paulo-win <xtbr295@gmail.com>
2025-07-26 15:45:06 -0300

    feat(futcamp/): main setup

---
+++ src/test/java/ifma/ppaulo/futcamp/FutcampApplicationTests.java
@@ -0,0 +1,13 @@
+package ifma.ppaulo.futcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutcampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
